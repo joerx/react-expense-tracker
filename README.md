6fba64b72dc66dd495b69a7a0dd3913b29ceec0c
Joerg Henning <1712460+joerx@users.noreply.github.com>
2020-08-29 23:19:37 +0800

    Update job status after cache buster

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ Based on React expense tracker app on YouTube with some additional components ad
 
 ## Components
 
+- Frontend: ReactJS app based on TBD
+- Backend: MongoDB backend, for practice, not deployed atm
+- SAM App: Serverless backend using DynamoDB based on AWS SAM
+- CDK: CDK stacks for frontend (S3, CloudFront) and Code Pipelines
+
 ## Notes
 
 ### Repo Layout
@@ -12,3 +17,10 @@ Based on React expense tracker app on YouTube with some additional components ad
 - In real live you may need to split them up
 - Downside is that each push to any component would trigger redeploys to all the others
 - E.g. changes to backend would also redeploy the frontend & invalidate the CDN cache
+
+### GITHUB_TOKEN
+
+- GITHUB_TOKEN must be set in local env
+- Doesn't exist in the CodePipeline isn't needed at that point
+- Still, since all stacks are instantiated, the token must exist (even an empty value will do)
+- Better option might be to store the token in SecretsManager and always read it from there
--- cdk/lib/cache-buster/index.js
+++ cdk/lib/cache-buster/index.js
@@ -3,20 +3,58 @@ const AWS = require("aws-sdk");
 const distId = process.env.CF_DISTRIBUTION_ID;
 
 const cloudfront = new AWS.CloudFront();
+const codepipeline = new AWS.CodePipeline();
 
-exports.handler = async (event, context) => {
-  console.log(event);
+const putJobSuccess = async (jobId) => {
+  if (!jobId) {
+    console.log("No job id, skipping job notification");
+    return;
+  }
+
+  await codepipeline.putJobSuccessResult({ jobId }).promise();
+};
+
+const putJobFailure = async (jobId, message, context) => {
+  if (!jobId) {
+    console.log("No job id, skipping job notification");
+    return;
+  }
 
   const params = {
-    DistributionId: distId,
-    InvalidationBatch: {
-      CallerReference: Date.now().toString(),
-      Paths: {
-        Quantity: "1",
-        Items: ["/*"],
-      },
+    jobId: jobId,
+    failureDetails: {
+      message: JSON.stringify(message),
+      type: "JobFailed",
+      externalExecutionId: context.awsRequestId,
     },
   };
 
-  return await cloudfront.createInvalidation(params).promise();
+  await codepipeline.putJobFailureResult(params).promise();
+};
+
+exports.handler = async (event, context) => {
+  let jobId = null;
+  if (event["CodePipeline.job"]) {
+    jobId = event["CodePipeline.job"].id;
+    console.log("Event for jobId", jobId);
+  }
+
+  try {
+    const params = {
+      DistributionId: distId,
+      InvalidationBatch: {
+        CallerReference: Date.now().toString(),
+        Paths: {
+          Quantity: "1",
+          Items: ["/*"],
+        },
+      },
+    };
+
+    const res = await cloudfront.createInvalidation(params).promise();
+    await putJobSuccess(jobId);
+    return res;
+  } catch (err) {
+    await putJobFailure(jobId, err.message, context);
+  }
 };
