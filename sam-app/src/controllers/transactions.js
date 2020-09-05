18b3179c52b7e0753c27ebb4ee3d1081d9e870ab
Joerg Henning <1712460+joerx@users.noreply.github.com>
2020-09-05 19:01:40 +0800

    Add xray

--- sam-app/src/controllers/transactions.js
+++ sam-app/src/controllers/transactions.js
@@ -1,4 +1,5 @@
-const AWS = require("aws-sdk");
+const AWSXRay = require("aws-xray-sdk-core");
+const AWS = AWSXRay.captureAWS(require("aws-sdk"));
 const { v4: uuidv4 } = require("uuid");
 
 const awsOptions = {
